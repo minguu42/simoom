f27d43427b43110df9b57e9b9fd5e0ebef57bc81
minguu42 <minguu42@gmail.com>
2023-09-23 07:59:13 +0900

    ファイルを分割する

--- pkg/infra/mysql/mysql.go
+++ pkg/infra/mysql/mysql.go
@@ -54,17 +54,3 @@ func NewClient(conf env.MySQL) (*Client, error) {
 
 	return &Client{db: db}, nil
 }
-
-func newNullTime(t *time.Time) sql.NullTime {
-	if t != nil {
-		return sql.NullTime{Time: *t, Valid: true}
-	}
-	return sql.NullTime{Valid: false}
-}
-
-func newPtrTime(t sql.NullTime) *time.Time {
-	if t.Valid {
-		return &t.Time
-	}
-	return nil
-}
