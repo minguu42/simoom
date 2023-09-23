131137b513107ed7054998477aaf111fc50ef04f
minguu42 <minguu42@gmail.com>
2023-09-23 21:47:21 +0900

    Tagモデルを追加する

--- pkg/domain/repository/repository.go
+++ pkg/domain/repository/repository.go
@@ -20,6 +20,7 @@ type Repository interface {
 
 	CreateTask(ctx context.Context, t model.Task) error
 	ListTasksByProjectID(ctx context.Context, projectID string, limit, offset uint) ([]model.Task, error)
+	ListTasksByTagID(ctx context.Context, tagID string, limit, offset uint) ([]model.Task, error)
 	GetTaskByID(ctx context.Context, id string) (model.Task, error)
 	UpdateTask(ctx context.Context, t model.Task) error
 	DeleteTask(ctx context.Context, id string) error
