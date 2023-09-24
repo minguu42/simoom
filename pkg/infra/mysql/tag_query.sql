42b4d1f7ebddaced2654559eb2b99682e213666c
minguu42 <minguu42@gmail.com>
2023-09-24 20:30:58 +0900

    タグ関連のSQLファイルを追加する

---
+++ pkg/infra/mysql/tag_query.sql
@@ -0,0 +1,30 @@
+-- name: CreateTag :exec
+INSERT INTO tag (id, user_id, name, created_at, updated_at)
+VALUES (?, ?, ?, ?, ?);
+
+-- name: ListTagsByUserID :many
+SELECT *
+FROM tag
+WHERE user_id = ?
+LIMIT ? OFFSET ?;
+
+-- name: ListTagsByTaskID :many
+SELECT t.*
+FROM tag AS t
+       INNER JOIN task_tag AS tt ON t.id = tt.tag_id
+WHERE tt.task_id = ?;
+
+-- name: GetTagByID :one
+SELECT *
+FROM tag
+WHERE id = ?;
+
+-- name: UpdateTag :exec
+UPDATE tag
+SET name = ?
+WHERE id = ?;
+
+-- name: DeleteTag :exec
+DELETE
+FROM tag
+WHERE id = ?;
