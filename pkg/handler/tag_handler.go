9ed1b72f113f8aba2863a7919bb084b55b4c9c03
Akira Furukawa <minguu42@gmail.com>
2023-10-08 16:54:54 +0900

    add tag usecase

--- pkg/handler/tag_handler.go
+++ pkg/handler/tag_handler.go
@@ -2,14 +2,11 @@ package handler
 
 import (
 	"context"
-	"time"
 
 	"connectrpc.com/connect"
-	"github.com/cockroachdb/errors"
 	"github.com/minguu42/simoom/gen/simoompb/v1"
-	"github.com/minguu42/simoom/pkg/domain/idgen"
 	"github.com/minguu42/simoom/pkg/domain/model"
-	"github.com/minguu42/simoom/pkg/domain/repository"
+	"github.com/minguu42/simoom/pkg/usecase"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -32,73 +29,50 @@ func newTagsResponse(ts []model.Tag) []*simoompb.TagResponse {
 }
 
 type tagHandler struct {
-	repo repository.Repository
+	uc usecase.TagUsecase
 }
 
 func (h tagHandler) CreateTag(ctx context.Context, req *connect.Request[simoompb.CreateTagRequest]) (*connect.Response[simoompb.TagResponse], error) {
-	now := time.Now()
-	t := model.Tag{
-		ID:        idgen.Generate(),
-		UserID:    userID,
-		Name:      req.Msg.Name,
-		CreatedAt: now,
-		UpdatedAt: now,
+	out, err := h.uc.CreateTag(ctx, usecase.CreateTagInput{
+		Name: req.Msg.Name,
+	})
+	if err != nil {
+		return nil, err
 	}
-	if err := h.repo.CreateTag(ctx, t); err != nil {
-		return nil, errInternal
-	}
-	return nil, errInternal
+	return connect.NewResponse(newTagResponse(out.Tag)), nil
 }
 
-func (h tagHandler) ListTags(ctx context.Context, req *connect.Request[simoompb.ListTagsRequest]) (*connect.Response[simoompb.TagsResponse], error) {
-	ts, err := h.repo.ListTagsByUserID(ctx, userID, 100, 0)
+func (h tagHandler) ListTags(ctx context.Context, _ *connect.Request[simoompb.ListTagsRequest]) (*connect.Response[simoompb.TagsResponse], error) {
+	out, err := h.uc.ListTags(ctx, usecase.ListTagsInput{
+		Limit:  10,
+		Offset: 0,
+	})
 	if err != nil {
-		return nil, errInternal
+		return nil, err
 	}
 	return connect.NewResponse(&simoompb.TagsResponse{
-		Tags: newTagsResponse(ts),
+		Tags: newTagsResponse(out.Tags),
 	}), nil
 }
 
 func (h tagHandler) UpdateTag(ctx context.Context, req *connect.Request[simoompb.UpdateTagRequest]) (*connect.Response[simoompb.TagResponse], error) {
-	t, err := h.repo.GetTagByID(ctx, req.Msg.Id)
-	if err != nil {
-		if errors.Is(err, repository.ErrModelNotFound) {
-			return nil, errTagNotFound
-		}
-		return nil, errInternal
-	}
-	if t.UserID != userID {
-		return nil, errInternal
-	}
-
 	if req.Msg.Name == nil {
 		return nil, errInvalidArgument
 	}
-	if req.Msg.Name != nil {
-		t.Name = *req.Msg.Name
-	}
-	if err := h.repo.UpdateTag(ctx, t); err != nil {
-		return nil, errInternal
-	}
 
-	return connect.NewResponse(newTagResponse(t)), nil
+	out, err := h.uc.UpdateTag(ctx, usecase.UpdateTagInput{
+		ID:   req.Msg.Id,
+		Name: req.Msg.Name,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return connect.NewResponse(newTagResponse(out.Tag)), nil
 }
 
 func (h tagHandler) DeleteTag(ctx context.Context, req *connect.Request[simoompb.DeleteTagRequest]) (*connect.Response[emptypb.Empty], error) {
-	t, err := h.repo.GetTagByID(ctx, req.Msg.Id)
-	if err != nil {
-		if errors.Is(err, repository.ErrModelNotFound) {
-			return nil, errTagNotFound
-		}
-		return nil, errInternal
-	}
-	if t.UserID != userID {
-		return nil, errInternal
-	}
-
-	if err := h.repo.DeleteTag(ctx, req.Msg.Id); err != nil {
-		return nil, errInternal
+	if err := h.uc.DeleteTag(ctx, usecase.DeleteTagInput{ID: req.Msg.Id}); err != nil {
+		return nil, err
 	}
 	return connect.NewResponse(&emptypb.Empty{}), nil
 }
