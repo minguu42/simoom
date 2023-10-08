a2863f3c0b08aa78f7bfd3632615b4f8bde21ef7
Akira Furukawa <minguu42@gmail.com>
2023-10-08 16:08:22 +0900

    add task usecase

--- pkg/usecase/usecase.go
+++ pkg/usecase/usecase.go
@@ -6,5 +6,8 @@ const userID = "01DXF6DT000000000000000000"
 
 var (
 	ErrProjectNotFound = errors.New("the specified project is not found")
+	ErrStepNotFound    = errors.New("the specified step is not found")
+	ErrTagNotFound     = errors.New("the specified tag is not found")
+	ErrTaskNotFound    = errors.New("the specified task is not found")
 	ErrUnkown          = errors.New("an unintentional error occurred on the server")
 )
