8731f5e709eb9ee38b44f6906c5d0a4ff614cfb5
Akira Furukawa <minguu42@gmail.com>
2023-10-15 20:08:01 +0900

    change table name

--- mysql/initdb/initdb.sh
+++ mysql/initdb/initdb.sh
@@ -5,7 +5,7 @@ db_name="simoomdb"
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${db_name}"
 mysql -u root ${db_name} < /schema.sql
 mysql -u root ${db_name} <<EOF
-INSERT INTO user (id, name, api_key, created_at, updated_at)
+INSERT INTO users (id, name, api_key, created_at, updated_at)
 VALUES ('01DXF6DT000000000000000000', 'minguu42', 'rAM9Fm9huuWEKLdCwHBcju9Ty_-TL2tDsAicmMrXmUnaCGp3RtywzYpMDPdEtYtR', '2020-01-01 00:00:00', '2020-01-01 00:00:00')
 EOF
 
