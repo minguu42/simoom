2d3532cbb680193c332d72d6b1046284698c144b
Akira Furukawa <minguu42@gmail.com>
2023-11-06 20:46:12 +0900

    Authenticatorインタフェースを追加する

---
+++ pkg/domain/auth/authenticator.go
@@ -0,0 +1,11 @@
+package auth
+
+import "github.com/minguu42/simoom/pkg/domain/model"
+
+// Authenticator はユーザ認証を抽象化する
+type Authenticator interface {
+	CreateAccessToken(user model.User, secret string, expiry int) (string, error)
+	CreateRefreshToken(user model.User, secret string, expiry int) (string, error)
+	IsAuthorized(tokenString string, secret string) (bool, error)
+	ExtractIDFromToken(tokenString string, secret string) (string, error)
+}
