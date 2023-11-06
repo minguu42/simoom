84c7e91266f1bbdeaddd8d8d1e92d40fe96475a9
Akira Furukawa <50062855+minguu42@users.noreply.github.com>
2023-11-06 20:25:23 +0900

    認証系のハンドラにバリデーションを追加する (#21)
    * signUpとsignInを入れ替える
    
    * 認証系のハンドラにバリデーションを追加する
    
    * auth_handler_test.goを追加する
    
    * ファイル名を変更する

--- pkg/usecase/auth_usecase.go
+++ pkg/usecase/auth_usecase.go
@@ -18,40 +18,6 @@ type AuthUsecase struct {
 	Env  config.Env
 }
 
-type SignInInput struct {
-	Email    string
-	Password string
-}
-
-type SignInOutput struct {
-	AccessToken  string
-	RefreshToken string
-}
-
-func (u AuthUsecase) SignIn(ctx context.Context, in SignInInput) (SignInOutput, error) {
-	user, err := u.Repo.GetUserByEmail(ctx, in.Email)
-	if err != nil {
-		return SignInOutput{}, errors.WithStack(err)
-	}
-
-	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(in.Password)) != nil {
-		return SignInOutput{}, errors.New("password is not valid")
-	}
-
-	accessToken, err := auth.CreateAccessToken(user, u.Env.API.AccessTokenSecret, u.Env.API.AccessTokenExpiryHour)
-	if err != nil {
-		return SignInOutput{}, errors.WithStack(err)
-	}
-	refreshToken, err := auth.CreateRefreshToken(user, u.Env.API.RefreshTokenSecret, u.Env.API.RefreshTokenExpiryHour)
-	if err != nil {
-		return SignInOutput{}, errors.WithStack(err)
-	}
-	return SignInOutput{
-		AccessToken:  accessToken,
-		RefreshToken: refreshToken,
-	}, nil
-}
-
 type SignUpInput struct {
 	Name     string
 	Email    string
@@ -90,6 +56,40 @@ func (u AuthUsecase) SingUp(ctx context.Context, in SignUpInput) (SignUpOutput, 
 		return SignUpOutput{}, errors.WithStack(err)
 	}
 	return SignUpOutput{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}, nil
+}
+
+type SignInInput struct {
+	Email    string
+	Password string
+}
+
+type SignInOutput struct {
+	AccessToken  string
+	RefreshToken string
+}
+
+func (u AuthUsecase) SignIn(ctx context.Context, in SignInInput) (SignInOutput, error) {
+	user, err := u.Repo.GetUserByEmail(ctx, in.Email)
+	if err != nil {
+		return SignInOutput{}, errors.WithStack(err)
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(in.Password)) != nil {
+		return SignInOutput{}, errors.New("password is not valid")
+	}
+
+	accessToken, err := auth.CreateAccessToken(user, u.Env.API.AccessTokenSecret, u.Env.API.AccessTokenExpiryHour)
+	if err != nil {
+		return SignInOutput{}, errors.WithStack(err)
+	}
+	refreshToken, err := auth.CreateRefreshToken(user, u.Env.API.RefreshTokenSecret, u.Env.API.RefreshTokenExpiryHour)
+	if err != nil {
+		return SignInOutput{}, errors.WithStack(err)
+	}
+	return SignInOutput{
 		AccessToken:  accessToken,
 		RefreshToken: refreshToken,
 	}, nil
