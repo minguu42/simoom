6be42b43837933ac1252c22571ea29da491f318f
Akira Furukawa <minguu42@gmail.com>
2023-11-08 20:23:07 +0900

    TODOコメントを追加する

--- pkg/usecase/project_usecase_test.go
+++ pkg/usecase/project_usecase_test.go
@@ -97,7 +97,7 @@ func TestProjectUsecase_ListProjects(t *testing.T) {
 						UpdatedAt:  time.Date(2020, 1, 1, 0, 0, 2, 0, time.UTC),
 					},
 				},
-				HasNext: false,
+				HasNext: false, // TODO: ページングの実装後に対応する
 			},
 		},
 	}
