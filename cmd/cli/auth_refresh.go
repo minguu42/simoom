b5439d78d358f99f0ed5060f0b4984e4a1a62cad
Akira Furukawa <50062855+minguu42@users.noreply.github.com>
2023-11-14 20:58:08 +0900

    authコマンドを実装する (#28)
    * cmdutilパッケージを追加する
    
    * coreを伝搬させる
    
    * logを削除する
    
    * signupサブコマンドを実装する
    
    * エラーメッセージを修正する
    
    * signinサブコマンドを実装する
    
    * refreshサブコマンドを実装する

--- cmd/cli/auth_refresh.go
+++ cmd/cli/auth_refresh.go
@@ -1,22 +1,51 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 
+	"connectrpc.com/connect"
+	"github.com/cockroachdb/errors"
+	"github.com/minguu42/simoom/cmd/cli/cmdutil"
+	"github.com/minguu42/simoom/gen/simoompb/v1"
 	"github.com/spf13/cobra"
 )
 
-func newCmdAuthRefresh() *cobra.Command {
-	return &cobra.Command{
+type authRefreshOpts struct {
+	refreshToken string
+}
+
+func newCmdAuthRefresh(core cmdutil.Core) *cobra.Command {
+	opts := authRefreshOpts{}
+	cmd := &cobra.Command{
 		Use:   "refresh",
 		Short: "Refresh the access token",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return runAuthRefresh()
+			if opts.refreshToken == "" {
+				return errors.New("refresh-token is required")
+			}
+			return runAuthRefresh(cmd.Context(), core, opts)
 		},
 	}
+
+	cmd.Flags().StringVar(&opts.refreshToken, "refresh-token", "", "refresh token")
+
+	return cmd
 }
 
-func runAuthRefresh() error {
-	fmt.Println("runAuthRefresh executed")
+func runAuthRefresh(ctx context.Context, core cmdutil.Core, opts authRefreshOpts) error {
+	resp, err := core.Client.RefreshAccessToken(ctx, connect.NewRequest(&simoompb.RefreshAccessTokenRequest{
+		RefreshToken: opts.refreshToken,
+	}))
+	if err != nil {
+		return fmt.Errorf("failed to call RefreshAccessToken method. %w", err)
+	}
+
+	data, err := json.MarshalIndent(resp.Msg, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal: %w", err)
+	}
+	fmt.Println(string(data))
 	return nil
 }
