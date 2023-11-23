84f337e5a392028d319776da8c2cd432e96be169
Akira Furukawa <minguu42@gmail.com>
2023-11-23 19:56:45 +0900

    taskパッケージを追加する

--- cli/root.go
+++ cli/root.go
@@ -5,6 +5,7 @@ import (
 	"github.com/minguu42/simoom/cli/cmd/project"
 	"github.com/minguu42/simoom/cli/cmd/step"
 	"github.com/minguu42/simoom/cli/cmd/tag"
+	"github.com/minguu42/simoom/cli/cmd/task"
 	"github.com/minguu42/simoom/cli/cmdutil"
 	"github.com/spf13/cobra"
 )
@@ -19,6 +20,6 @@ func newCmdRoot(core cmdutil.Core) *cobra.Command {
 	cmd.AddCommand(project.NewCmdProject(core))
 	cmd.AddCommand(step.NewCmdStep(core))
 	cmd.AddCommand(tag.NewCmdTag(core))
-	cmd.AddCommand(newCmdTask(core))
+	cmd.AddCommand(task.NewCmdTask(core))
 	return cmd
 }
