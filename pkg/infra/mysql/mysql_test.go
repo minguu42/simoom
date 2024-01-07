797dd34b719565d56540930e4cff0bc4d57fd510
Akira Furukawa <minguu42@gmail.com>
2024-01-07 12:50:17 +0900

    mysqlパッケージのテストにTestcontainers for Goを導入する

--- pkg/infra/mysql/mysql_test.go
+++ pkg/infra/mysql/mysql_test.go
@@ -1,19 +1,47 @@
 package mysql
 
 import (
+	"context"
 	"log"
 	"testing"
 
 	"github.com/minguu42/simoom/pkg/config"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 var tc *Client
 
 func TestMain(m *testing.M) {
-	var err error
+	ctx := context.Background()
+	mysqlC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: "mysql:8.0.32",
+			Env: map[string]string{
+				"MYSQL_DATABASE":             "testdb",
+				"MYSQL_ALLOW_EMPTY_PASSWORD": "yes",
+			},
+			ExposedPorts: []string{"3306/tcp"},
+			WaitingFor:   wait.ForListeningPort("3306/tcp"),
+		},
+		Started: true,
+	})
+	if err != nil {
+		log.Fatalf("failed to create mysql container: %s", err)
+	}
+	defer func() {
+		if err := mysqlC.Terminate(ctx); err != nil {
+			log.Fatalf("failed to terminate mysql container: %s", err)
+		}
+	}()
+
+	port, err := mysqlC.MappedPort(ctx, "3306/tcp")
+	if err != nil {
+		log.Fatalf("failed to get externally mapped port: %s", err)
+	}
 	tc, err = NewClient(config.DB{
 		Host:               "localhost",
-		Port:               3306,
+		Port:               port.Int(),
 		Database:           "testdb",
 		User:               "root",
 		Password:           "",
@@ -26,6 +54,7 @@ func TestMain(m *testing.M) {
 	}
 	defer tc.Close()
 
+	Migrate(tc)
 	InitAllData(tc)
 
 	m.Run()
