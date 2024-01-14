af291365c3df4014ff9e26be2c6c17a1ae77b9e8
Akira Furukawa <minguu42@gmail.com>
2024-01-15 08:18:23 +0900

    S3バックエンドを作成する

---
+++ infra/terraform/mutable/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.32.1"
+    }
+  }
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  default_tags {
+    tags = {
+      Product   = local.product
+      Env       = var.env
+      Owner     = "minguu42"
+      ManagedBy = "terraform"
+    }
+  }
+}
+
+variable "env" {
+  type = string
+  validation {
+    condition     = contains(["prod", "stg"], var.env)
+    error_message = "Only the following environment is allowed: prod | stg"
+  }
+}
+
+locals {
+  product      = "simoom"
+  isProduction = var.env == "prod"
+}
