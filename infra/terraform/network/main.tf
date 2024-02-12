cec957d285a150d8945c1db4e7bad39897a1c077
Akira Furukawa <minguu42@gmail.com>
2024-02-12 19:46:51 +0900

    networkに出力関数を追加する

--- infra/terraform/network/main.tf
+++ infra/terraform/network/main.tf
@@ -33,3 +33,11 @@ locals {
   product      = "simoom"
   isProduction = var.env == "prod"
 }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "private_subnet_ids" {
+  value = [aws_subnet.private_a.id, aws_subnet.private_c.id]
+}
