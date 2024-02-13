2a3b309b8b806f2f882f3ee12d1373954ebb0399
Akira Furukawa <minguu42@gmail.com>
2024-02-14 08:07:52 +0900

    出力関数public_subnet_idsを追加する

--- infra/terraform/network/main.tf
+++ infra/terraform/network/main.tf
@@ -38,6 +38,10 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
+output "public_subnet_ids" {
+  value = [aws_subnet.public_a.id, aws_subnet.public_c.id]
+}
+
 output "public_a_subnet_id" {
   value = aws_subnet.public_a.id
 }
