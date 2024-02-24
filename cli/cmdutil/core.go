64e6501ec7c17494c6984c22b65ecb3c4e7c856c
Akira Furukawa <minguu42@gmail.com>
2024-02-24 21:10:30 +0900

    認証ファイルを扱う関数を追加する

--- cli/cmdutil/core.go
+++ cli/cmdutil/core.go
@@ -2,11 +2,6 @@ package cmdutil
 
 import "github.com/minguu42/simoom/lib/go/simoompb/v1/simoompbconnect"
 
-type Credentials struct {
-	AccessToken  string
-	RefreshToken string
-}
-
 type Core struct {
 	Client      simoompbconnect.SimoomServiceClient
 	Credentials Credentials
