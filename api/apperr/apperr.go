6d2d1968006712bc04501f6daff2003bf344ff82
Akira Furukawa <minguu42@gmail.com>
2024-03-03 13:04:25 +0900

    apperrパッケージを追加する

---
+++ api/apperr/apperr.go
@@ -0,0 +1,13 @@
+// Package apperr はアプリケーション独自のエラーを定義する
+// このパッケージで定義されたエラーは handler パッケージと usecase パッケージから利用する
+package apperr
+
+import "errors"
+
+var (
+	ErrProjectNotFound = errors.New("the specified project is not found")
+	ErrStepNotFound    = errors.New("the specified step is not found")
+	ErrTagNotFound     = errors.New("the specified tag is not found")
+	ErrTaskNotFound    = errors.New("the specified task is not found")
+	ErrUserNotFound    = errors.New("the specified user is not found")
+)
