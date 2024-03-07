184eabc6559b11202083b956d023ffc6f56363e9
Akira Furukawa <minguu42@gmail.com>
2024-03-07 20:37:19 +0900

    factoryパッケージを追加する

---
+++ cli/factory/factory.go
@@ -0,0 +1,37 @@
+package factory
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/minguu42/simoom/cli/api"
+)
+
+type Factory struct {
+	Profile string
+	Client  api.Client
+}
+
+// New は Factory を生成する
+func New(profile string) (*Factory, error) {
+	c, err := api.NewClient(profile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create api client: %w", err)
+	}
+	return &Factory{
+		Profile: profile,
+		Client:  c,
+	}, nil
+}
+
+type factoryKey struct{}
+
+// Value はコンテキストから Factory を取り出す
+func Value(ctx context.Context) *Factory {
+	return ctx.Value(factoryKey{}).(*Factory)
+}
+
+// WithFactory は Factory を含めたコンテキストを返す
+func WithFactory(ctx context.Context, f *Factory) context.Context {
+	return context.WithValue(ctx, factoryKey{}, f)
+}
