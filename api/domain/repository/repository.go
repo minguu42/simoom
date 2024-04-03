b684a2cd2e692a7ac207b10b4954b95083145b99
Akira Furukawa <minguu42@gmail.com>
2024-04-03 21:33:00 +0900

    Repositoryのインタフェースを変更する

--- api/domain/repository/repository.go
+++ api/domain/repository/repository.go
@@ -17,30 +17,30 @@ type Repository interface {
 	Transaction(ctx context.Context, fn func(ctxWithTx context.Context) error) error
 
 	CreateProject(ctx context.Context, p model.Project) error
-	ListProjectsByUserID(ctx context.Context, userID string, limit, offset uint) ([]model.Project, error)
-	GetProjectByID(ctx context.Context, id string) (model.Project, error)
+	ListProjectsByUserID(ctx context.Context, userID model.UserID, limit, offset uint) ([]model.Project, error)
+	GetProjectByID(ctx context.Context, id model.ProjectID) (model.Project, error)
 	UpdateProject(ctx context.Context, p model.Project) error
-	DeleteProject(ctx context.Context, id string) error
+	DeleteProject(ctx context.Context, id model.ProjectID) error
 
 	CreateTask(ctx context.Context, t model.Task) error
-	ListTasksByUserID(ctx context.Context, userID string, limit, offset uint, projectID, tagID *string) ([]model.Task, error)
-	GetTaskByID(ctx context.Context, id string) (model.Task, error)
+	ListTasksByUserID(ctx context.Context, userID model.UserID, limit, offset uint, projectID *model.ProjectID, tagID *model.TagID) ([]model.Task, error)
+	GetTaskByID(ctx context.Context, id model.TaskID) (model.Task, error)
 	UpdateTask(ctx context.Context, t model.Task) error
-	DeleteTask(ctx context.Context, id string) error
+	DeleteTask(ctx context.Context, id model.TaskID) error
 
 	CreateStep(ctx context.Context, s model.Step) error
-	GetStepByID(ctx context.Context, id string) (model.Step, error)
+	GetStepByID(ctx context.Context, id model.StepID) (model.Step, error)
 	UpdateStep(ctx context.Context, s model.Step) error
-	DeleteStep(ctx context.Context, id string) error
+	DeleteStep(ctx context.Context, id model.StepID) error
 
 	CreateTag(ctx context.Context, t model.Tag) error
-	ListTagsByUserID(ctx context.Context, userID string, limit, offset uint) ([]model.Tag, error)
-	GetTagByID(ctx context.Context, id string) (model.Tag, error)
+	ListTagsByUserID(ctx context.Context, userID model.UserID, limit, offset uint) ([]model.Tag, error)
+	GetTagByID(ctx context.Context, id model.TagID) (model.Tag, error)
 	UpdateTag(ctx context.Context, t model.Tag) error
-	DeleteTag(ctx context.Context, id string) error
+	DeleteTag(ctx context.Context, id model.TagID) error
 
 	CreateUser(ctx context.Context, u model.User) error
-	GetUserByID(ctx context.Context, id string) (model.User, error)
+	GetUserByID(ctx context.Context, id model.UserID) (model.User, error)
 	GetUserByName(ctx context.Context, name string) (model.User, error)
 	GetUserByEmail(ctx context.Context, email string) (model.User, error)
 }
