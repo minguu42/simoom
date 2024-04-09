dbfeaf6a559995fcca9ea85146ab795224814569
Akira Furukawa <minguu42@gmail.com>
2024-04-09 09:21:32 +0900

    Config構造体を移動させる

--- api/config/config.go
+++ api/config/config.go
@@ -15,3 +15,36 @@ func Load() (Config, error) {
 	}
 	return conf, nil
 }
+
+// Config はアプリケーションで使用する設定値
+type Config struct {
+	API  API
+	Auth Auth
+	DB   DB
+}
+
+// API はAPIに関する設定
+type API struct {
+	Host string `envconfig:"API_HOST" default:"0.0.0.0"`
+	Port int    `envconfig:"API_PORT" default:"8080"`
+}
+
+// Auth は認証に関する設定
+type Auth struct {
+	AccessTokenExpiryHour  int    `envconfig:"ACCESS_TOKEN_EXPIRY_HOUR" default:"2"`
+	RefreshTokenExpiryHour int    `envconfig:"REFRESH_TOKEN_EXPIRY_HOUR" default:"168"`
+	AccessTokenSecret      string `envconfig:"ACCESS_TOKEN_SECRET" required:"true"`
+	RefreshTokenSecret     string `envconfig:"REFRESH_TOKEN_SECRET" required:"true"`
+}
+
+// DB はデータベースに関する設定
+type DB struct {
+	Host               string `envconfig:"DB_HOST" required:"true"`
+	Port               int    `envconfig:"DB_PORT" required:"true"`
+	Database           string `envconfig:"DB_DATABASE" required:"true"`
+	User               string `envconfig:"DB_USER" required:"true"`
+	Password           string `envconfig:"DB_PASSWORD" required:"true"`
+	ConnMaxLifetimeMin int    `envconfig:"DB_CONN_MAX_LIFETIME_MIN" default:"5"`
+	MaxOpenConns       int    `envconfig:"DB_MAX_OPEN_CONNS" default:"25"`
+	MaxIdleConns       int    `envconfig:"DB_MAX_IDLE_CONNS" default:"25"`
+}
