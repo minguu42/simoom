37849657ef95dcf73925131bed15ad2d16ecdde8
Akira Furukawa <minguu42@gmail.com>
2024-06-04 21:20:38 +0900

    アクセスログの出力項目を増やす

--- api/logging/logging.go
+++ api/logging/logging.go
@@ -2,7 +2,6 @@ package logging
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
 	"os"
 	"time"
@@ -53,22 +52,49 @@ func Error(ctx context.Context, msg string) {
 	logger(ctx).Log(ctx, slog.LevelError, msg)
 }
 
+type AccessFields struct {
+	// レスポンス関連のフィールド
+	ExecutionTime time.Duration
+	Err           error
+
+	// リクエスト関連のフィールド
+	HTTPMethod    string
+	Service       string
+	Method        string
+	ContentLength int
+	RemoteAddr    string
+}
+
 // Access はアクセスログを出力する
-func Access(ctx context.Context, method string, executionTime time.Duration, err error) {
-	if err == nil { // if NO error
-		logger(ctx).LogAttrs(ctx, slog.LevelInfo, fmt.Sprintf("ok (0) %s", method),
-			slog.Int64("execution_time", executionTime.Milliseconds()),
+func Access(ctx context.Context, fields AccessFields) {
+	message := "Request is accepted"
+	executionTimeAttr := slog.Int64("execution_time", fields.ExecutionTime.Milliseconds())
+	requestAttr := slog.Group("request",
+		slog.String("http_method", fields.HTTPMethod),
+		slog.String("service", fields.Service),
+		slog.String("method", fields.Method),
+		slog.Int("content_length", fields.ContentLength),
+		slog.String("remote_address", fields.RemoteAddr),
+	)
+
+	if fields.Err == nil { // if NO error
+		logger(ctx).LogAttrs(ctx, slog.LevelInfo, message,
+			executionTimeAttr,
+			requestAttr,
 		)
 		return
 	}
 
-	appErr := apperr.NewError(err)
+	appErr := apperr.NewError(fields.Err)
 	level := slog.LevelWarn
 	if appErr.Code() == connect.CodeUnknown {
 		level = slog.LevelError
 	}
-	logger(ctx).LogAttrs(ctx, level, fmt.Sprintf("%s (%[1]d) %s", appErr.Code(), method),
-		slog.Int64("execution_time", executionTime.Milliseconds()),
-		slog.String("detail", err.Error()),
+	logger(ctx).LogAttrs(ctx, level, message,
+		executionTimeAttr,
+		requestAttr,
+		slog.Int("error_code", int(appErr.Code())),
+		slog.String("error_text", appErr.Code().String()),
+		slog.String("error_message", appErr.Error()),
 	)
 }
